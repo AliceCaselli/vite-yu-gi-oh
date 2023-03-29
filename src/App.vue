980d99132a9db32f2e1a701f9ca32c7c37cc1ad5
AliceCaselli <alicecaselli3@gmail.com>
2023-03-29 09:15:24 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+
+import AppHeader from "./components/AppHeader.vue";
+import AppCards from "./components/AppCards.vue";
+
+export default {
+  data() {
+    return {};
+  },
+
+  components: {
+    AppHeader,
+    AppCards,
+  },
+};
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+  <AppCards></AppCards>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,62 @@
+<script>
+
+name: 'AppHeader';
+
+export default {
+    data() {
+        return {
+
+            links: [
+                'the game',
+                'the cards',
+                'the story',
+
+            ],
+        };
+    },
+};
+</script>
+
+<template>
+    <header>
+        <div id="logo">
+            <img src="/images/logo.png" alt="">
+
+        </div>
+
+        <div id="list">
+            <ul>
+                <li v-for="link in links">{{ link }}</li>
+            </ul>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    width: 100vw;
+    background-color: #f6f6f6;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+
+    #list {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+
+        ul {
+            width: 100%;
+            display: flex;
+            flex-direction: row;
+            gap: 50px;
+
+            color: black;
+            list-style-type: none;
+            text-transform: uppercase;
+            font-weight: bold;
+        }
+    }
+}
+</style>
